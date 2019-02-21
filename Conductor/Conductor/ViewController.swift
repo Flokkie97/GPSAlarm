eaf5fbaf2655b3b05866970cc92cf0a4bd8a465a
Carsten Flokstra <C.Flokstra@student.han.nl>
2019-02-21 09:28:53 +0100

    Initial commit

---
+++ Conductor/Conductor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conductor
+//
+//  Created by Carsten Flokstra on 21/02/2019.
+//  Copyright © 2019 Carsten Flokstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
